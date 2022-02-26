36b7a16bd7f614dea626f48ea5951991a1443cc1
Rocinate <764952936@qq.com>
2022-02-26 09:54:35 +0800

    del useless code file

--- PolyCentroid.m
+++
@@ -1,8 +0,0 @@
-function [Cx,Cy] = PolyCentroid(X,Y)
-Xa = [X(2:end);X(1)];
-Ya = [Y(2:end);Y(1)];
-
-A = 1/2*sum(X.*Ya-Xa.*Y); %signed area of the polygon
-
-Cx = (1/(6*A)*sum((X + Xa).*(X.*Ya-Xa.*Y)));
-Cy = (1/(6*A)*sum((Y + Ya).*(X.*Ya-Xa.*Y)));
